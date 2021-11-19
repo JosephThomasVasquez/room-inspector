d6d78b951be80c320132a0922222fa4b2afaf355
Joseph Thomas Vasquez <joetv.tech@gmail.com>
2021-11-18 19:52:19 -0800

    Added Home route

--- src/Layout/index.js
+++ src/Layout/index.js
@@ -1,10 +1,17 @@
 import React from "react";
+import { Routes, Route, Switch } from "react-router-dom";
 import Header from "./Header";
+import Home from "./Home";
 
 const Layout = () => {
   return (
     <div>
       <Header />
+      <div className="container">
+        <Routes>
+          <Route path="/" element={<Home />} />
+        </Routes>
+      </div>
     </div>
   );
 };
