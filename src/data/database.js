1c0cc5d56177e4beb7450d2a1f3f8b0e8ccbedf9
Joseph Thomas Vasquez <joetv.tech@gmail.com>
2021-11-19 06:01:00 -0800

    Added boilerplate components

---
+++ src/data/database.js
@@ -0,0 +1,28 @@
+const data = {
+  rooms: [
+    {
+      id: 0,
+      name: "Name",
+      roomType: "Room Type",
+      building: "Building",
+      floor: "Floor",
+      area: "Area",
+      checked: false,
+      images: [],
+    },
+  ],
+
+  users: [
+    {
+      id: 0,
+      username: "Username",
+      email: "Email",
+      title: "Admin",
+      isAdmin: true,
+      userId: "@dmin",
+    },
+  ],
+  settings: [],
+};
+
+export default data;
---
+++ src/data/options.js
@@ -0,0 +1,13 @@
+const options = {
+  id: 0,
+  title: "Title",
+  description: "Description",
+  type: "Type",
+  notificationType: "Notification Type",
+  department: "Department",
+  recipient: "Recipient",
+  notes: "Notes",
+  roomId: 0,
+};
+
+export default options;
