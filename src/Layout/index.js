f930418deb8dfe181895d472752faf36fbd03271
Joseph Thomas Vasquez <joetv.tech@gmail.com>
2021-11-19 06:28:08 -0800

    Changed database to json

--- src/Layout/index.js
+++ src/Layout/index.js
@@ -1,15 +1,24 @@
-import React from "react";
+import React, { useState } from "react";
 import { Routes, Route, Switch } from "react-router-dom";
 import Header from "./Header";
 import Home from "./Home";
+import RoomList from "./Rooms/RoomsList";
 
 const Layout = () => {
+  const [rooms, setRooms] = useState([]);
+
+  const getRooms = async (signal) => {
+    try {
+      const response = await fetch();
+    } catch (error) {}
+  };
   return (
     <div>
       <Header />
       <div className="container">
         <Routes>
-          <Route path="/" element={<Home />} />
+          <Route path="/" element={<RoomList />} />
+          {/* <Route path="/rooms" element={<Home />} /> */}
         </Routes>
       </div>
     </div>
